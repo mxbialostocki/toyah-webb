5bd410c5c761a885cc76e64a7d0d220796930d6f
Matt Bialostocki <matt.bialostocki@gmail.com>
2019-05-25 14:02:59 +1200

    Main renders two links to poems

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom'
 function Main () {
   return (
     <React.Fragment>
-      <h2>Render the Main</h2>
-      <Link to="/killkistkreydy">
-        <div className="entry-frames">
-        </div>
-      </Link>
-      <Link to="/raggedrobin">
-        <div className="entry-frames">
-        </div>
-      </Link>
+      <div className="main-container">
+        <Link to="/killkistkreydy">
+          <div className="entry-frames">
+            <h1>killkist kreydy</h1>
+          </div>
+        </Link>
+        <Link to="/raggedrobin">
+          <div className="entry-frames">
+            <h1>ragged robin</h1>
+          </div>
+        </Link>
+      </div>
     </React.Fragment>
   )
 }
