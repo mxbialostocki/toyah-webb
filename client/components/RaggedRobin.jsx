ea709fc08770ad2a65c70b9189fe5034e5e4d5b7
Matt Bialostocki <matt.bialostocki@gmail.com>
2019-05-27 10:27:07 +1200

    import original ragged robin html

--- client/components/RaggedRobin.jsx
+++ client/components/RaggedRobin.jsx
@@ -3,7 +3,167 @@ import React from 'react'
 class RaggedRobin extends React.Component {
   render () {
     return (
-      <div><h2>renders RaggedRobbin</h2></div>
+      <React.Fragment>
+        {/* <div><h2>renders RaggedRobbin</h2></div> */}
+        <header>
+          <div className="container980">
+            <h1>ragged robin</h1>
+            <div className="cheeks">
+              <figure className="cheek"></figure>
+              <figure className="cheek"></figure>
+            </div>
+          </div>
+        </header>
+
+        <main>
+          <section className="rrpoemi">
+            <div className="container980">
+              <section className="acti">
+                <h2><a href="#">Act II</a></h2>
+                <div className="descend">
+                  <h3>&#40;The Fiction Suit called Ragged Robin emerges from the L... T&#41;</h3>
+                  <h4>Robin&sol;&sol;</h4>
+                  <div className="actilines"><span>P</span><span>e</span><span>e</span><span>l</span><span>e</span><span>d</span>
+                    <span>b</span><span>a</span><span>c</span><span>k</span> <span>h</span><span>e</span><span>r</span>
+                    <span>e</span><span>y</span><span>e</span><span>l</span><span>i</span><span>d</span><span>s</span>
+                  </div>
+                  <h5>try to remember</h5>
+                  <h4>Robin&sol;&sol;</h4>
+                  <p>It was different before</p>
+                  <p>Underneath hexagonal slits</p>
+                  <p alt="the I dissolution">Before SC</p>
+                  <p>The cusp of an unknowable</p>
+                  <p>&apos;Personalities&apos; we called them</p>
+                  <p>A tear in our facet</p>
+                  <h4>Rob-&sol;&sol;TRY TO REMEMBER!-Robin&sol;&sol;</h4>
+                  <p>Scaffolding only necessary for growth and was later husked</p>
+                  <p>There are no gaps between letters</p>
+                  <p>Even before ID two hemispheres of the brain began talking to each other</p>
+                  <p>A serum made of IIs and OOOs</p>
+                  <p>It does not exist for us</p>
+                  <p>Suddenly, utterable</p>
+                  <h5>try to remember&excl;&excl;</h5>
+                </div>
+              </section>
+            </div>
+          </section>
+
+          <section className="rrpoemi">
+            <div className="container980">
+              <section className="actii">
+                <h2>Act OOO</h2>
+                <h3>&#40;Who is Ragged Robin? Language begins a disjointed dialogue with itself&#41;</h3>
+                <h4>Robin&sol;&sol;</h4>
+                <p className="right"><span>These symbols are too socialized</span></p>
+                <p className="right"><span>Does the story go on without me?</span></p>
+                <h5 className="right"><span className="small">the senten</span><span className="medium">ce</span> <span className="large">is</span> <span className="xlarge">up</span></h5>
+                <h4>Robin&sol;&sol;</h4>
+                <p className="right"><span>Better than the LT though, not so bright</span></p>
+                <p className="right"><span>In this form there is both ...&#9679; &#9679; and absence. Invisible sentences do exist</span></p>
+                <h5 className="right">&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;&period;</h5>
+                <p><span>A dream where it all shrinks to a pinhole</span></p>
+                <p><span>Or is it the other way around?</span></p>
+                <p><span>The pinhole is a spot of Language caught between two slats and escaping</span></p>
+                <p className="right"><span>Who is telling me?</span></p>
+                <p><span>How many people have to tell a true story before it becomes true?</span></p>
+                <h5 className="left">&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;&lowbar;</h5>
+                <p><span>The whole experience of assimilation is dizzy</span></p>
+              </section>
+            </div>
+          </section>
+
+          <section className="rrpoemi">
+            <div className="container980">
+              <section className="actiii">
+                <h2>Act Dec{ /* nets of anti-speech */ }tasee</h2>
+                <div className="priestess">{ /* of the outerchurch comes home to roost */ }
+                  <h3>&#40;Ragged Robin feels the language-cortex of her … &#9679; losing its grasp&#41;</h3>
+                  <h4>Robin&sol;&sol;</h4>
+                  <p className="outerchurch">th e i n   c u lat i o n w a s pla n te  d</p>
+                  <p className="innerchurch">This form is so unnatural!</p>
+                  <p>C&#61;a&#61;l&#61;l&#61;e&#61;d i&#61;t r&#61;o&#61;s&#61;w&#61;e&#61;l&#61;l</p>
+                  <p className="innerchurch">Everything from the outside. Time/space an X-Ray</p>
+                  <p>&apos;Now&apos; is one segment of &apos;You&apos;</p>
+                  <p className="outerchurch">A ll i  s in t h e va l ue of  t he ga me</p>
+                  <p className="innerchurch">There is no …….</p>
+                  <p>L&#61;a&#61;y&#61;e&#61;r&#61;s a&#61;n&#61;d l&#61;a&#61;y&#61;e&#61;r&#61;s o&#61;f a&#61;r&#61;t&#61;i&#61;f&#61;a&#61;c&#61;e</p>
+                  <p className="outerchurch">K  no wle d   g  e a  s Li q u i  dI  nf or m  a   t  i on</p>
+
+                  <p className="innerchurch">Instructions to breathe!</p>
+                  <p className="innerchurch">…… &#9679; has a roof and a door but no walls</p>
+                </div>
+              </section>
+            </div>
+            <div className="deci">
+              <p className="right">&#60;&#47;&lowbar;translate&lowbar;these&lowbar;commands  &lowbar;&#47;	&#62;</p>
+              <p className="right">&#60;&#47; &quot;&lowbar;t&lowbar;o&lowbar;t&lowbar;e&lowbar;p   &lowbar;&quot; &#47;	&#62;</p>
+            </div>
+            <div className="decii">
+              <p className="right">&#60;&#47;&lowbar;translate&lowbar;these&lowbar;commands  &lowbar;&#47;	&#62;</p>
+              <p className="right">&#60;&#47; &quot;&lowbar;t&lowbar;o&lowbar;t&lowbar;e&lowbar;p   &lowbar;&quot; &#47;	&#62;</p>
+            </div>
+          </section>
+
+          <section className="rrpoemi">
+            <section className="activ">
+              <h2>Act Totep</h2>
+              <div className="activbin">
+                <span className="binary">01010000</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01001001</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01001111</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01000011</span>
+                <span className="binary">01001010</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01001101</span>
+                <span className="binary">01000001</span>
+              </div>
+              <div className="activbin">
+                <span className="binary">01010000</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01001001</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01001111</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01000011</span>
+                <span className="binary">01001010</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01001101</span>
+                <span className="binary">01000001</span>
+              </div>
+              <div className="activbin">
+                <span className="binary">01010000</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01001001</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01001111</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01010010</span>
+                <span className="binary">01010011</span>
+                <span className="binary">01000011</span>
+                <span className="binary">01001010</span>
+                <span className="binary">01001110</span>
+                <span className="binary">01000101</span>
+                <span className="binary">01001101</span>
+                <span className="binary">01000001</span>
+              </div>
+            </section>
+
+          </section>
+        </main>
+      </React.Fragment>
+
     )
   }
 }
